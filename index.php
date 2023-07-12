c59b9acee919f088627dc5e11504c9b98194773c
Mounir-Bmz <mboumaza.mounir@gmail.com>
2023-07-12 02:54:46 +0200

    Header responsive DONE

--- index.php
+++ index.php
@@ -2,25 +2,23 @@
 
 <body>
 
-  <section id="accueil">
-    <h2>Bienvenue sur mon portfolio !</h2>
-    <!-- Ajoute ici une brève introduction sur toi -->
-  </section>
-
-  <section id="projets">
-    <h2>Mes Projets</h2>
-    <!-- Ajoute ici tes projets avec des descriptions, des captures d'écran, des liens, etc. -->
-  </section>
-
-  <section id="apropos">
-    <h2>À propos de moi</h2>
-    <!-- Ajoute ici des informations sur ton parcours, tes compétences, etc. -->
-  </section>
-
-  <section id="contact">
-    <h2>Contact</h2>
-    <!-- Ajoute ici un formulaire de contact ou des informations de contact -->
-  </section>
+  <main aria-label="Content">
+    <div class="container">
+      <div class="home">
+        <div class="py-3 my-auto text-center">
+          <img class="d-block mx-auto mb-4" src="assets/images/avatar-2.png" alt="Avatar de Mounir Boumaza" />
+          <h1 class="display-5 fw-bold">Mr Boumaza, Mounir Boumaza</h1>
+          <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">Développeur web et animateur à mes heures perdues</p>
+            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+              <a href="https://www.linkedin.com/in/mounir-boumaza/" class="btn btn-primary btn-lg px-4 gap-3">Linkedin</a>
+              <a href="https://github.com/Mounir-Bmz#my-projects-" class="btn btn-outline-secondary btn-lg px-4">Mes projets</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 
 </body>
 
--- public/templates/header.tpl.php
+++ public/templates/header.tpl.php
@@ -11,13 +11,13 @@
 
 <header>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <h1 class="navbar-brand">Mon Portfolio</h1>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <div class="container-fluid d-flex justify-content-center align-items-center">
+      <h1 class="navbar-brand mr-auto my-2">Mounir Boumaza</h1>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+      <div class="collapse navbar-collapse justify-content-center text-center" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
           <li class="nav-item">
             <a class="nav-link" href="#accueil">Accueil</a>
           </li>
@@ -35,4 +35,14 @@
     </div>
   </nav>
 </header>
+
+
+
+
+
+
+
+
+
+
 
