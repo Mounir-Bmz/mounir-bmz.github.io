a1ce3eb8d1c933f4ce736913ab0fb0f4ff292e62
Mounir-Bmz <mboumaza.mounir@gmail.com>
2023-07-28 15:17:08 +0200

    Correction navbar-expand

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,13 +1,17 @@
 // Selection du bouton dark mode
 const darkModeButton = document.getElementById('darkModeButton');
-const elementBG = document.querySelector('.BG');
-const footer = document.querySelector('footer.BG');
+const navElement = document.querySelector("nav.BG");
+const footerElement = document.querySelector("footer.BG");
 
 // Fonction mode sombre
 function enableDarkMode() {
   document.body.classList.add('dark-mode');
-  elementBG.classList.add('dark-mode');
-  footer.classList.add('dark-mode');
+  navElement.classList.add('dark-mode');
+  footerElement.classList.add('dark-mode');
+
+  navElement.classList.remove('navbar-light');
+  navElement.classList.add('navbar-dark');
+  
 
   // On change l'image en dark
   const avatarImage = document.querySelector('.img-fluid');
@@ -17,8 +21,11 @@ function enableDarkMode() {
 // Fonction delet mode sombre
 function disableDarkMode() {
   document.body.classList.remove('dark-mode');
-  elementBG.classList.remove('dark-mode');
-  footer.classList.remove('dark-mode');
+  navElement.classList.remove('dark-mode');
+  footerElement.classList.remove('dark-mode');
+
+  navElement.classList.remove('navbar-dark');
+  navElement.classList.add('navbar-light');
 
   // On change l'image en light
   const avatarImage = document.querySelector('.img-fluid');
